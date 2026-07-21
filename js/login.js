d48c2611c091ae5bb0b4cb115304aeb1bcbf2a72
Kherlysson Ryann <106851981+TKRyann@users.noreply.github.com>
2026-07-21 10:26:56 -0300

    fix: corrigir erro de digitação na obtenção do elemento de email

--- js/login.js
+++ js/login.js
@@ -1,4 +1,7 @@
 const formLogin = document.getElementById("form-login");
-const email = document.getElementsById("email");
+const email = document.getElementById("email");
 const senha = document.getElementById("senha");
 const checkboxMostraSenha = document.getElementById("checkbox-mostrar-senha");
+const mensagemLogin = document.getElementById("mensagem-login");
+
+console.log(email);
